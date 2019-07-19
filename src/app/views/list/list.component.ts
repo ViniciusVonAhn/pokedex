583ab5b532ab43058e5666d29e36895edb9d23d1
ViniciusVonAhn <viniciusvonahn@gmail.com>
2019-07-19 18:01:02 -0300

    6-1 OK

--- src/app/views/list/list.component.ts
+++ src/app/views/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokeapiService } from 'src/app/services/pokeapi.service';
 
 @Component({
   selector: 'app-list',
@@ -6,17 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  nameFilter = '';
+  selectedPkm = null;
+  get pokemonList(){
+    return this.pokeapi.pokeList.filter(pokemon => {
+      return pokemon.name.toLowerCase().indexOf(this.nameFilter.toLowerCase()) !== -1;
+    })
+  }
 
-  pokemonList = [
-    { name: 'Bulbasaur', number: 1 },
-    { name: 'Charmander', number: 4 },
-    { name: 'Squirtle', number: 7 },
-    { name: 'Pikachu', number: 25 }
-  ]
-
-  constructor() { }
+  constructor(
+    private pokeapi: PokeapiService
+  ) { }
 
   ngOnInit() {
+  }
+
+  selectPokemon(pkm){
+    this.selectedPkm = pkm;
+  }
+
+  get pkmSprite(){
+    const number = ('000' + this.selectedPkm.number).slice(-3);
+    return `//serebii.net/sunmoon/pokemon/${number}.png`;
   }
 
 }
