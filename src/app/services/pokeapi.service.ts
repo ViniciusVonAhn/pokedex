eea625be59716f694e5b56662926c2a241cc88b8
ViniciusVonAhn <viniciusvonahn@gmail.com>
2019-07-21 17:48:57 -0300

    FIX - pokedex finish

--- src/app/services/pokeapi.service.ts
+++ src/app/services/pokeapi.service.ts
@@ -1,16 +1,46 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+interface pokeListResponse{
+  created: string,
+  modified: string,
+  name: string,
+  pokemon: any[],
+  resource_uri: string
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokeapiService {
 
-  pokeList = [
-    { name: 'Bulbasaur', number: 1 },
-    { name: 'Charmander', number: 4 },
-    { name: 'Squirtle', number: 7 },
-    { name: 'Pikachu', number: 25 }
-  ]
+  private url = '//dev.treinaweb.com.br/pokeapi';
 
-  constructor() { }
+  pokeList = [];
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  lisAll(){
+    this.http.get<pokeListResponse>(`${this.url}/pokedex/1`)
+      .subscribe(
+        response => {
+          response.pokemon.forEach(pokemon => {
+            pokemon.number = this.getNumberFromUrl(pokemon.resource_uri);
+          })
+          this.pokeList = this.sortPokemon(response.pokemon).filter(pokemon => pokemon.number < 1000)
+        }
+      )
+  }
+
+  private getNumberFromUrl(url){
+    return parseInt(url.replace(/.*\/(\d+)\/$/,'$1'));
+  }
+
+  private sortPokemon(pokeList){
+    return pokeList.sort((a, b) => {
+      return (a.number > b.number ? 1 : -1);
+    })
+  }
 }
